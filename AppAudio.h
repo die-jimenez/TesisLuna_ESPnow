0f410a553078280e15a1a1f8d4a0397d14571934
Diego Jiménez <diegojimenez2010@gmail.com>
2026-03-31 20:50:04 -0300

    nuevo audioApp pero sin pruebas

--- AppAudio.h
+++ AppAudio.h
@@ -1,124 +1,70 @@
-//Library versions are detailed in the README
 #include "Arduino.h"
-#include "src/StatueStateMachine.h"
 #include "src/DeltaTime/DeltaTime.h"
-
 #include "src/AudioPlayer/AudioPlayer.h"
 #include "src/CustomEspNow/CustomEspNow.h"
 #include "src/StatueSetting/StatueSetting.h"
 
-
-#define RXD2 16  // esp receiver (rx) --> module transciever (tx)
-#define TXD2 17  // D2 = segundo puerto serial, pero es solo nombre
+#define RXD2 16
+#define TXD2 17
 #define BUSY 4
 
-DeltaTime deltaTime;
-StatueStateMachine statueStateMachine;
-GlobalStateMachine globalStateMachine;
-
-
-#pragma region->Parametros modificables
-//===================================================
-#define STATUE_SAD //--------------------------------> CAMBIAR ESTA VARIABLE PARA CAMBIAR LAS ESTATUAS =>
+#define STATUE_SAD  //--------------------------------> CAMBIAR PARA CAMBIAR ESTATUA
 
 #ifdef STATUE_HAPPY
-
+StatueSetting statueSetting(StatueSetting::Name::AUDIO_HAPPY);
 #endif
-
 #ifdef STATUE_SAD
-
+StatueSetting statueSetting(StatueSetting::Name::AUDIO_SAD);
 #endif
 
-#pragma endregion
-//===========================================================================================================
+bool wasPlaying = false;
 
-
-
-
-//En un "h" -> C++ lee se arriba para abajo
-//===================================================
 void OnReceiveData(const EspNowMessage& data);
-void OnSendData(const EspNowMessage& data);
-void OnAudioFinished();
-void OnPettingStarted();
-//===================================================
 
-
-//Métodos Principales
-//===================================================
 void Audio_Setup() {
   Serial.begin(115200);
-  Serial.println("Iniciando audio");
+  Serial.println("Iniciando ESP-Audio");
   delay(10000);
 
-  //Global state machine
-  globalStateMachine.Init(&statueSetting, &statueStateMachine, &deltaTime);
-
-  //State Machine
-  statueStateMachine.Init(&statueSetting, &sensorsManager, &lights, &deltaTime);
-  statueStateMachine.RegisterOnPettingStarted(OnPettingStarted);
-  statueStateMachine.RegisterOnAudioFinished(OnAudioFinished);
-  statueStateMachine.SetCanInteract(true);
-
-  //Esp now
   EspNowInit();
-  EspNowRegisterOnSend(OnSendData);
   EspNowRegisterOnReceive(OnReceiveData);
 
-  //Audio module
   Mp3ModuleInit(RXD2, TXD2, BUSY);
   delay(500);
-  PlaySound(1);
 
-  delay(1000);
-  Serial.println("setup is complete");
+  Serial.println("Setup completo");
   Serial.println("--------------------------------------------");
 }
 
 void Audio_Loop() {
+  bool playing = IsPlayingAudio();
 
-
-
-  //Maquina de estados de la estatua
-  //===================================================
-  if (statueStateMachine.GetCanInteract()) {
-    if (statueStateMachine.state == StatueStateMachine::IDLE) {
-      statueStateMachine.UpdateIdle();
-    }  //
-    else if (statueStateMachine.state == StatueStateMachine::INTERACTING) {
-      statueStateMachine.UpdateInteraction(pettingTriggerTime, MIN_SENSORS_ACTIVE_TO_PET);
-    }  //
-    else if (statueStateMachine.state == StatueStateMachine::PETTING) {
-      statueStateMachine.UpdatePetting();
-    }
+  if (wasPlaying && !playing) {
+    Serial.println("Audio terminado, avisando al ESP-Sensores");
+    EspNowSetAndSendMessage(
+      (int)statueSetting.name,  // AUDIO_HAPPY o AUDIO_SAD
+      0,
+      BOTH_ENABLED,
+      false,
+      false  // toAudio = false, va al ESP-Sensores
+    );
   }
+  wasPlaying = playing;
 
-  if (!statueStateMachine.GetCanInteract()) {
-    lights.Update(Lights::LightState::OFF, &deltaTime);
-  }
-  globalStateMachine.UpdateResetTimer(&INACTIVITY_TIMEOUT, &BAD_ENDING_RESET_TIMEOUT);
-
-  deltaTime.Run();
   delay(5);
 }
-//===========================================================================================================
 
-
-
-//Estos son los metodos custom para utilizar CustomEspNow
 void OnReceiveData(const EspNowMessage& data) {
-  globalStateMachine.OnReciveMessage(data);
-}
+  if (!data.toAudio) return;
 
-void OnSendData(const EspNowMessage& data) {
-  globalStateMachine.OnSendMessage(data);
-}
+  Serial.print("Reproduciendo track para stage: ");
+  Serial.println(data.stage);
 
-void OnAudioFinished() {
-  globalStateMachine.OnAudioFinished();
+  switch (data.stage) {
+    case 0: PlaySound(StatueSetting::AudiosTrack::TRACK_PURR_COMPLAIN); break;
+    case 1: PlaySound(StatueSetting::AudiosTrack::TRACK_SONG_1); break;
+    case 2: PlaySound(StatueSetting::AudiosTrack::TRACK_SONG_2); break;
+    case 3: PlaySound(data.isReadyToHappyEnding ? StatueSetting::AudiosTrack::TRACK_GOOD_ENDING : StatueSetting::AudiosTrack::TRACK_BAD_ENDING); break;
+  }
+  DelayForBusyUpdate();
 }
-
-void OnPettingStarted() {
-  globalStateMachine.OnPettingStarted();
-}
-
